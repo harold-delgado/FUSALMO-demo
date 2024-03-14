98b79f8c0e047d4235441886a99832c565de325d
harold-delgado1 <harold.delgado@telus.com>
2024-03-14 09:02:06 -0600

    First release fixes

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 "use strict";
 
-var input = document.getElementById('input'), // input/output button
-  number = document.querySelectorAll('.numbers div'), // number buttons
-  operator = document.querySelectorAll('.operators div'), // operator buttons
-  result = document.getElementById('result'), // equal button
-  clear = document.getElementById('clear'), // clear button
-  resultDisplayed = false; // flag to keep an eye on what output is displayed
+let input = document.getElementById('input'); // input/output button
+let number = document.querySelectorAll('.numbers div'); // number buttons
+let operator = document.querySelectorAll('.operators div'); // operator buttons
+let result = document.getElementById('result'); // equal button
+let clear = document.getElementById('clear'); // clear button
+let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
-// adding click handlers to number buttons
-for (var i = 0; i < number.length; i++) {
+// adding event listeners for the buttons
+for (let i = 0; i < number.length; i++) {
   number[i].addEventListener("click", function(e) {
-
-    // storing current input string and its last character in variables - used later
-    var currentString = input.innerHTML;
-    var lastChar = currentString[currentString.length - 1];
+    let currentString = input.innerHTML;
+    let lastChar = currentString[currentString.length - 1];
 
     // if result is not diplayed, just keep adding
     if (resultDisplayed === false) {
@@ -30,21 +28,18 @@ for (var i = 0; i < number.length; i++) {
       input.innerHTML = "";
       input.innerHTML += e.target.innerHTML;
     }
-
   });
 }
 
 // adding click handlers to number buttons
-for (var i = 0; i < operator.length; i++) {
+for (let i = 0; i < operator.length; i++) {
   operator[i].addEventListener("click", function(e) {
-
-    // storing current input string and its last character in variables - used later
-    var currentString = input.innerHTML;
-    var lastChar = currentString[currentString.length - 1];
+    let currentString = input.innerHTML;
+    let lastChar = currentString[currentString.length - 1];
 
     // if last character entered is an operator, replace it with the currently pressed one
     if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
-      var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+      let newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
       input.innerHTML = newString;
     } else if (currentString.length == 0) {
       // if first key pressed is an opearator, don't do anything
@@ -57,51 +52,38 @@ for (var i = 0; i < operator.length; i++) {
   });
 }
 
-// on click of 'equal' button
 result.addEventListener("click", function() {
+  let inputString = input.innerHTML;
+  let numbers = inputString.split(/\+|\-|\×|\÷/g);
+  let operators = inputString.replace(/[0-9]|\./g, "").split("");
 
-  // this is the string that we will be processing eg. -10+26+33-56*34/23
-  var inputString = input.innerHTML;
-
-  // forming an array of numbers. eg for above string it will be: numbers = ["10", "26", "33", "56", "34", "23"]
-  var numbers = inputString.split(/\+|\-|\×|\÷/g);
-
-  // forming an array of operators. for above string it will be: operators = ["+", "+", "-", "*", "/"]
-  // first we replace all the numbers and dot with empty string and then split
-  var operators = inputString.replace(/[0-9]|\./g, "").split("");
-
-  console.log(inputString);
+  /* console.log(inputString);
   console.log(operators);
   console.log(numbers);
-  console.log("----------------------------");
+  console.log("----------------------------"); */
 
-  // now we are looping through the array and doing one operation at a time.
-  // first divide, then multiply, then subtraction and then addition
-  // as we move we are alterning the original numbers and operators array
-  // the final element remaining in the array will be the output
-
-  var divide = operators.indexOf("÷");
+  let divide = operators.indexOf("÷");
   while (divide != -1) {
     numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
     operators.splice(divide, 1);
     divide = operators.indexOf("÷");
   }
 
-  var multiply = operators.indexOf("×");
+  let multiply = operators.indexOf("×");
   while (multiply != -1) {
     numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
     operators.splice(multiply, 1);
     multiply = operators.indexOf("×");
   }
 
-  var subtract = operators.indexOf("-");
+  let subtract = operators.indexOf("-");
   while (subtract != -1) {
     numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
     operators.splice(subtract, 1);
     subtract = operators.indexOf("-");
   }
 
-  var add = operators.indexOf("+");
+  let add = operators.indexOf("+");
   while (add != -1) {
     // using parseFloat is necessary, otherwise it will result in string concatenation :)
     numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
@@ -114,7 +96,6 @@ result.addEventListener("click", function() {
   resultDisplayed = true; // turning flag if result is displayed
 });
 
-// clearing the input on press of clear
 clear.addEventListener("click", function() {
   input.innerHTML = "";
 })
