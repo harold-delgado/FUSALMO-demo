827b786412d6b2898573687689ccda64b658fb3a
AndreaLuna2004 <andrealuna2004@icloud.com>
2024-03-15 11:33:27 -0600

    División Arreglada

--- script.js
+++ script.js
@@ -100,5 +100,5 @@ function multiplyFn(num1, num2) {
 }
 
 function divideFn(num1, num2) {
-  throw new Error('Esta feature aun no existe! Suscribete a calculadora premium para poder obtener acceso a esta funcion');
+  return num1 / num2;
 }
